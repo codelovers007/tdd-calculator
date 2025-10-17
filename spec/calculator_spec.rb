1999cfa002396be9e79bb452948b0bc0c49be32b
Niraj <ndharwal624@gmail.com>
2025-10-17 12:39:03 +0530

    Test case for multiple numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -14,5 +14,9 @@ RSpec.describe Calculator do
     it 'returns the sum of two numbers' do
       expect(Calculator.add('1,2')).to eq(3)
     end
+
+    it 'returns the sum of multiple numbers' do
+      expect(Calculator.add('1,2,3')).to eq(6)
+    end
   end
 end
