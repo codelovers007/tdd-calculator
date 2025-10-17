7ce5a7c25f0734252fb61b3a967cff4822df808a
Niraj <ndharwal624@gmail.com>
2025-10-17 12:37:50 +0530

    Test case if string have two numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe Calculator do
     it 'returns the number itself if a single number' do
       expect(Calculator.add('1')).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(Calculator.add('1,2')).to eq(3)
+    end
   end
 end
